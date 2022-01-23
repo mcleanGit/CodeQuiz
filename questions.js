826a7338758ddadca1fc52000975deb28034ff86
Don McLean <mclean@utoronto.ca>
2022-01-22 19:40:52 -0500

    updates to script, qscript option, not called

---
+++ questions.js
@@ -0,0 +1,53 @@
+/* question.js as separate file tbd */
+var questions = [
+ {
+   question: "1. Commonly used data types do not include:",
+   answer: "3. alerts",
+       options: [
+          "1. strings",
+          "2. booleans",
+          "3. alerts",
+          "4. numbers"
+      ]
+   },
+   {
+     question: "2. The condition of an if / else statement is enclosed with _____.",
+     answer: "2. curly brackets",
+     options: [
+           "1. quotes",
+           "2. curly brackets",
+           "3. parenthesis",
+           "4. square brackets"
+       ]
+   },
+   {
+     question: "3. Arrays in JavaScript can be used to store _____.",
+     answer: "4. all of the above",
+     options: [
+           "1. numbers",
+           "2. other arrays",
+           "3. booleans",
+           "4. all of the above"
+       ]
+   },
+   {
+       question: "4. String values must be enclosed within _____ when being assigned to variables.",
+       answer: "4. parentheses",
+       options: [
+           "1. commas",
+           "2. curly brackets",
+           "3. quotes",
+           "4. parentheses"
+       ]
+   },
+   {
+       question: "5. A very useful tool used during development and debugging for printing content to the debugger is:",
+       answer: "4. console log",
+       options: [
+           "1. JavaScript",
+           "2. terminal / bash",
+           "3. for loops",
+           "4. console log"
+       ]
+   }
+];
--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 /* script.js for Code Quiz - under development, adjust to mockup and task */
-// find, identify elements, set initial values, discussion with study-group+
+// find, identify elements, set initial values; discussion with study-group+; vars unused issues
 var startEl = document.querySelector("#start-quiz");
 var timerEl = document.getElementById("#countdown");
 var quizPage = document.querySelector(".quiz-page");
 var answerEl = document.getElementById("#answer-button");
 var questionIndex = 0;
 var timeLeft = 75;
-var resultsPage = document.querySelector(".results-page");
+var resultsPage = document.querySelector(".result-page");
 
+// had placed questions.js in here, now separate file
 //event Listener to start time
 document.querySelector("#start-quiz").addEventListener("click", function() {
-  // clear intro page section to begin quiz
+// clears / hides intro page section to start quiz, to review and confirm
   var element = document.getElementById("#intro");
   element.parentNode.removeChild(element);
-  // call timer function
+  // call timer function, call questions array sequence
   countdown();
-// ?
-// countdown function from 75 seconds, why the problem?
+  getQuestion ();
+});
+
+// countdown function from 75 seconds, not working: why the problem? placement in js?
 function countdown() {
-  var timeLeft = 75;
-  var timeInterval = setInterval(function () {
-    if (timeleft > 1) {
+  var timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
       timerEl.textContent = timeLeft + " seconds remaining";
       timeLeft--;
     } else if (timeLeft === 1) {
@@ -28,188 +30,141 @@ function countdown() {
       timeLeft--;
     } else {
       timerEl.textContent = "";
-      alert("Times up!");
       clearInterval(timeInterval);
+      alert("Time's up!");
     }
   }, 1000);
-
-  // stuff from below and then some goes in here.  Next revisits check and final
-  function checkAnswer(event) {
-    var userChoice = event.target.innerText
-    if (userChoice === questions[questionIndex].answer) {
-      alert("Correct!")
-    } else {
-      alert("Incorrect!")
-      timeLeft = -5; 
-    }
-    questionIndex++;
-    if (questionIndex < questions.length) {
-      getQuestion ();
-    } else {
-      clearInterval (timeInterval);
-      finalScore ();
-    }
-    }
-// remove last question and show results in separate page
-  function finalScore () {
-    var div = document.createElement("div");
-    var h1 = document.createElement("h1");
-    var p = document.createElement("p");
-    var p2 = document.createElement("p2");
-    var input = document.createElement("input");
-    var button = document.createElement("button");
-
-  // add content
-  h1.textContent = "Done!";
-  p.textContent = "Your final score is " + timerEl.textContent;
-  p2.textContent = "Enter initials: ";
-  input.textContent = "";
-  button.textContent = "Submit";
-
-  // put content on page
-  div.appendChild(h1);
-  div.appendChild(p);
-  div.appendChild(p2);
-  div.appendChild(input);
-  div.appendChild(button);
-  quizPage.innerHTML = "";
-  resultsPage.appendChild(div);
-
-/*
-  // other stuff from YouTube exx
-  const startButton = document.getElementbyId("start-btn");
-  const questionContainerElement = document.getElementById("question-container");
-  const questionElement = document.getElementbyId("question");
-  const answerButtonsElement = document.getElementById("answer-buttons");
-  let shuffledQuestions, currentQuestionIndex;
-
-  startButton.addEventListener("click", startQuiz);
-  nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    setNextQuestion();
-  });
-  /* to review fix */
-  function startQuiz() {
-    startButton.classList.add("hide");
-    questionContainerElement.classList.add("hide");
-    shuffledQuestions = question.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion();
-  }
-
-  function selectAnswer() {
-  }
-
-
-  startButton.classList.add("hide");
-
-  //nextQuestion not part of Challenge setup //
-  function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-  }
-  /* setting up the Q & A Arrays */
-  function showQuestion(question) {
-    questionElement.innerText = document.getElementById("question");
-    hover.getElementById("answer-btn");
-
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-      const button = document.createElement("button");
-      button.innerText = answer.text;
-      button.classList.add("btn");
-      if (answer.correct) {
-        button.dataset.correct = answer.correct;
-        button.addEventListener("click", selectAnswer);
-        answerButtonElement.appendChild("button");
-
-      }
-    }
-      /* was    else here or ? errors here as well */
-      ,
-      /* was    else here or ? errors here as well */
-      function resetState() {
-        clearStatusClass(document.body);
-        nextButton.classList.add("hide");
-        while (answerButtonElement.firstChild)
-          answerButtonsElement.removeChild;
-        hoverButtonElement.removeChild;
-        (answerButtonsElement.firstChild);
-      });
-
-    const nextButton = document.getElementById("next-btn");
-
-    selectAnswer(e); {
-      const selectedButton = e.target;
-      const correct = selectedButton.dataset.correct;
-      setStatusClass(document.body, correct);
-      Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
-      });
-      if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove("hide");
-      } else {
-        startButton.innerText = "Restart";
-        startButton.classList.remove("hide");
-      }
-    }
-
-  }
 }
-function setStatusClass (element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add ("correct")
+
+/* question.js possible as separate js file but here embedded in single js */
+var questions = [
+  {
+    question: "1. Commonly used data types do not include:",
+    answer: "3. alerts",
+      options: [
+        "1. strings",
+        "2. booleans",
+        "3. alerts",
+        "4. numbers"
+       ]
+    },
+    {
+      question: "2. The condition of an if / else statement is enclosed with _____.",
+      answer: "2. curly brackets",
+      options: [
+        "1. quotes",
+        "2. curly brackets",
+        "3. parenthesis",
+        "4. square brackets"
+        ]
+    },
+    {
+      question: "3. Arrays in JavaScript can be used to store _____.",
+      answer: "4. all of the above",
+      options: [
+        "1. numbers",
+        "2. other arrays",
+        "3. booleans",
+        "4. all of the above"
+        ]
+    },
+    {
+        question: "4. String values must be enclosed within _____ when being assigned to variables.",
+        answer: "4. parentheses",
+        options: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parentheses"
+        ]
+    },
+    {
+        question: "5. A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "4. console log",
+        options: [
+          "1. JavaScript",
+          "2. terminal / bash",
+          "3. for loops",
+          "4. console log"
+        ]
+    }
+ ];
+ 
+// creates question elements inside Javascript
+function getQuestion() {
+  var div = document.createElement("div");
+  var h1 = document.createElement("h1");
+  var buttonOne = document.createElement("answer-button");
+  var buttonTwo = document.createElement("answer-button");
+  var buttonThree = document.createElement("answer-button");
+  var buttonFour = document.createElement ("answer-button");
+
+// reads content of questions from above array
+h1.textContent = questions[questionIndex].question;
+buttonOne.textContent = questions[questionIndex].options[0];
+buttonTwo.textContent = questions[questionIndex].options[1];
+buttonThree.textContent = questions[questionIndex].options[2];
+buttonFour.textContent = questions[questionIndex].options[3];
+
+// EventListeners for answer buttons
+buttonOne.addEventListener("click", checkAnswer);
+buttonTwo.addEventListener("click", checkAnswer);
+buttonThree.addEventListener("click", checkAnswer);
+buttonFour.addEventListener("click", checkAnswer);
+
+// put content on page, to review this...
+div.appendChild("h1");
+div.appendChild("buttonOne");
+div.appendChild("buttonTwo");
+div.appendChild("buttonThree");
+div.appendChild("buttonFour");
+quizPage.innerHTML = "";
+quizPage.appendChild(div);
+}
+
+//check answers click-events for correct or incorrect
+function checkAnswer(event) {
+  var userChoice = event.target.innerText
+  if (userChoice === questions[questionIndex].answer) {
+    alert("Correct!")
   } else {
-    element.classList.add("wrong")
+    alert("Incorrect!")
+    timeLeft -= 5; 
+  }
+  questionIndex++;
+  if (questionIndex < questions.length) {
+    getQuestion ();
+  } else {
+    clearInterval(timeInterval);
+    finalScore();
   }
 }
 
-function clearStatusClass(element) {
-  element.classList.remove("correct")
-  element.classList.remove("wrong")
+function finalScore () {
+  var div = document.createElement("div");
+  var h1 = document.createElement("h1");
+  var p = document.createElement("p");
+  var p2 = document.createElement("p");
+  var input = document.createElement("input");
+  var button = document.createElement("button");
+
+// add content
+h1.textContent = "Done!";
+p.textContent = "Your final score is " + timerEl.textContent;
+p2.textContent = "Enter initials: ";
+input.textContent = "";
+button.textContent = "Submit";
+
+// put content on page
+div.appendChild("h1");
+div.appendChild("p");
+div.appendChild("p2");
+div.appendChild(input);
+div.appendChild(button);
+quizPage.innerHTML = "";
+resultsPage.appendChild("div");
 }
 
+/* final additional page with High scores function incomplete ?
+also clear high scores and go back reset/restart buttons */
 
-
-/*array of questions
-const questions = [
-  { 
-    question: "what is 2+2",
-    answers: [ 
-   {text: "4", correct: true},
-   {text: "22", correct: false},
-  ]
-  },
-  { 
-    question: "what is 3+3",
-    answers: [ 
-   {text: "6", correct: true},
-   {text: "33", correct: false},
-  ]
-  },
-  { 
-    question: "what is 4+4",
-    answers: [ 
-   {text: "8", correct: true},
-   {text: "44", correct: false},
-  ]
-  },
-  { 
-    question: "what is 5+5",
-    answers: [ 
-   {text: "10", correct: true},
-   {text: "55", correct: false},
-  ]
-  },
-  { 
-    question: "what is 6+6",
-    answers: [ 
-   {text: "12", correct: true},
-   {text: "66", correct: false},
-    ]
-  },
-]
-
-}
